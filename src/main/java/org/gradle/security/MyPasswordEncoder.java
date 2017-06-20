406829f9030ebfbbcfdd6d8bcc57041922779b43
haiwang2517 <wobunengmeiyou_ni@163.com>
2017-06-20 16:08:09 +0800

    抛出正确的密码不正确异常

--- src/main/java/org/gradle/security/MyPasswordEncoder.java
+++ src/main/java/org/gradle/security/MyPasswordEncoder.java
@@ -2,6 +2,7 @@ package org.gradle.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 
 public class MyPasswordEncoder extends ShaPasswordEncoder {
@@ -10,14 +11,25 @@ public class MyPasswordEncoder extends ShaPasswordEncoder {
 	
 	@Override
 	public String encodePassword(String rawPass, Object salt) {
-		logger.info("rawPass:["+rawPass+"],sal:["+salt+"]");
-		return super.encodePassword(rawPass, salt);
+		logger.info("加密");
+//		return super.encodePassword(rawPass, salt);
+		
+		//自定义密码加密,随便加个字母
+		return rawPass+"*1";
 	}
 
 	@Override
 	public boolean isPasswordValid(String encPass, String rawPass, Object salt) {
-		logger.info("encPass:["+encPass+"],rawPass:["+rawPass+"],salt:["+salt+"]");
-		return super.isPasswordValid(encPass, rawPass, salt);
+		logger.info("验证密码");
+//		return super.isPasswordValid(encPass, rawPass, salt);
+		
+		String retPass = encodePassword(rawPass,salt);
+		boolean passwordValid = retPass.equals(encPass);
+		if( !passwordValid ){
+			throw new BadCredentialsException("密码错误");
+		}
+		return passwordValid ;
 	}
+	
 
 }
